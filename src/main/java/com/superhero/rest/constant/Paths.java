8dd5e79504e838ee58f5f7fd20d4c8519f7ac54b
Igor-Philippov <45106977+Igor-Philippov@users.noreply.github.com>
2018-11-19 23:30:59 -0500

    Add files via upload

--- src/main/java/com/superhero/rest/constant/Paths.java
+++ src/main/java/com/superhero/rest/constant/Paths.java
@@ -1,13 +1,8 @@
 package com.superhero.rest.constant;
 
 public class Paths {
-	
-//    public static final String VERSION = "/api/1.0/";
-//    public static final String DEPARTURE = "departure/";
-//    public static final String ARRIVAL = "arrival/";
-//    public static final String FLIGHT = "flight/";
-//    public static final String USERS = "users/";
-    public static final String OD = "demo";
+
+	public static final String OD = "demo";
 	
     public static final String VERSION = "/api/1.0/";
 	
