30948e98a291049ea1f5e2fe09ec862eed922495
Igor-Philippov <45106977+Igor-Philippov@users.noreply.github.com>
2018-11-19 23:57:58 -0500

    Add files via upload

--- src/test/java/com/superhero/unit/controller/MissionControllerTest.java
+++ src/test/java/com/superhero/unit/controller/MissionControllerTest.java
@@ -1,69 +1,138 @@
 package com.superhero.unit.controller;
 
-//import static com.demo.constant.Paths.DEPARTURE;
-//import static com.demo.constant.Paths.VERSION;
+import static com.superhero.rest.constant.Paths.MISSIONS;
+import static com.superhero.rest.constant.Paths.MISSION_BY_ID;
 import static java.util.Collections.singletonList;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import com.superhero.unit.controller.BaseControllerTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-//import com.demo.controller.DepartureController;
-//import com.demo.domain.Departure;
+import com.superhero.model.Mission;
 import com.superhero.rest.controller.MissionController;
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(MissionController.class)
-public class MissionControllerTest {
-
-    @Autowired
-    private MockMvc mvc;
+public class MissionControllerTest extends BaseControllerTest {
+    Logger logger = LoggerFactory.getLogger(MissionControllerTest.class); 
+	
+	@Autowired
+    protected MockMvc mvc;
 
     @MockBean
     private MissionController missionController;
-
-    @Test
-    public void getAllDepartures() throws Exception {
-//        Departure departure = new Departure();
-//        departure.setCity("Yerevan");
-//
-//        List<Departure> departures = singletonList(departure);
-//
-//        given(departureController.getAllDepartures()).willReturn(departures);
-//
-//        mvc.perform(get(VERSION + DEPARTURE + "all")
-//                .with(user("blaze").password("Q1w2e3r4"))
-//                .contentType(APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", hasSize(1)))
-//                .andExpect(jsonPath("$[0].city", is(departure.getCity())));
+    
+    Mission mission = new Mission("mission1000"); {
+    	mission.setCompleted(true);
+    	mission.setId(1000L);
     }
-
+    
     @Test
-    public void getDepartureById() throws Exception {
-//        Departure departure = new Departure();
-//        departure.setCity("Yerevan");
-//
-//        given(departureController.getDepartureById(departure.getId())).willReturn(departure);
-//
-//        mvc.perform(get(VERSION + DEPARTURE + departure.getId())
-//                .with(user("blaze").password("Q1w2e3r4"))
-//                .contentType(APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("city", is(departure.getCity())));
+    public void retrieveMissionByIdTest() throws Exception {
+		given(missionController.retrieveMissionById(mission.getId())).willReturn(ResponseEntity.ok().body(mission));   	
+		mvc.perform(get(MISSION_BY_ID, mission.getId()))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("$.name", is(mission.getName())))
+				.andExpect(jsonPath("$.completed", is(mission.isCompleted())));
+    	
+		verify(missionController, times(1)).retrieveMissionById(mission.getId());
+		verifyNoMoreInteractions(missionController);    	
     }
+    
+	@Test
+	public void retrieveAllMissionsTest() throws Exception {
+		List<Mission> missions = singletonList(mission);
+		given(missionController.retrieveAllMissions()).willReturn(missions);
+		mvc.perform(get(MISSIONS).
+				contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(1)))
+				.andExpect(jsonPath("$[0].name", is(mission.getName())))
+				.andExpect(jsonPath("$[0].completed", is(mission.isCompleted())));
+		
+		verify(missionController, times(1)).retrieveAllMissions();
+		verifyNoMoreInteractions(missionController);  		
+	}
+    
+	@Test
+	public void createMissionTest() throws Exception {
+		//given(missionController.createMission(mission)).willReturn(ResponseEntity.ok().body(mission));
+		mvc.perform(post(MISSIONS)
+				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+				.content(toJsonString(mission)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].name", is(mission.getName())))
+				.andExpect(jsonPath("$[0].completed", is(mission.isCompleted())));
+	}
+	
+//	@Test
+//	public void retrieveAllMissionsTest() throws Exception {
+//
+//		List<Mission> missions = new ArrayList<Mission>();
+//		missions.add(mission);
+//
+//		when(missionResource.retrieveAllMissions()).thenReturn(missions);
+//
+//		mvc.perform(get("/missions/list").
+//				contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+//				.andExpect(status().isOk())
+//				.andExpect(jsonPath("$", hasSize(1)))
+//				.andExpect(jsonPath("$[0].name", is("mission")))
+//				.andExpect(jsonPath("$[0].isCompleted", is(false)))
+//				.andExpect(jsonPath("$[0].isDeleted", is(false)));
+//		
+//		verify(missionResource, times(1)).retrieveAllMissions();
+//		verifyNoMoreInteractions(missionResource);  		
+//
+//	}
+    
+    
+//	@Test
+//	public void updateMissionTest() throws Exception {
+//
+//		doNothing().when(missionResource).updateMission(mission, mission.getId());
+//
+//		mvc.perform(put("/missions/update/{id}", mission.getId())
+//				.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+//				.content(asJsonString(mission)))
+//				.andExpect(status().isOk());
+//
+//	} 
+    
+	@Test
+	public void deleteMissionTest() throws Exception {	
+		given(missionController.softDeleteMission(mission.getId())).willReturn(ResponseEntity.noContent().build());
+        mvc.perform(delete(MISSION_BY_ID, mission.getId())
+        		.with(user("shc_user").password("password"))
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isNoContent());
+		
+		verify(missionController, times(1)).softDeleteMission(mission.getId());
+		verifyNoMoreInteractions(missionController);
+	}
 }
--- src/test/java/com/superhero/unit/controller/SuperHeroControllerTest.java
+++ src/test/java/com/superhero/unit/controller/SuperHeroControllerTest.java
@@ -19,61 +19,16 @@ public class SuperHeroControllerTest {
 
     @MockBean
     private SuperHeroController superHeroController;
-    
-
-//    private Users createUser() {
-//        Users users = new Users();
-//        users.setName("BigG");
-//        users.setAddress("Glandale road 4");
-//        users.setCity("Glandale");
-//        users.setState("California");
-//        users.setZipCode("AD2123");
-//        users.setCardType("master");
-//        users.setCardNumber("2864528645765429346");
-//        users.setCardExpirationMonth(6);
-//        users.setCardExpirationYear(2020);
-//        users.setCardNameOnCard("BigGInDaHouse");
-//        users.setFlightId(12);
-//
-//        return users;
-//    }
 
     @Test
     public void getAllUsers() throws Exception {
-//        Users user = createUser();
-//
-//        List<Users> users = singletonList(user);
-//
-//        given(usersController.getAllUsers()).willReturn(users);
-//
-//        mvc.perform(get(VERSION + USERS + "all")
-//                .with(user("blaze").password("Q1w2e3r4"))
-//                .contentType(APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$", hasSize(1)))
-//                .andExpect(jsonPath("$[0].address", is(user.getAddress())));
     }
 
     @Test
     public void getUsersById() throws Exception {
-//        Users user = createUser();
-//
-//        given(usersController.getUsersById(user.getId())).willReturn(user);
-//
-//        mvc.perform(get(VERSION + USERS + user.getId())
-//                .with(user("blaze").password("Q1w2e3r4"))
-//                .contentType(APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("address", is(user.getAddress())));
     }
 
     @Test
     public void removeUsersById() throws Exception {
-//        Users user = createUser();
-//
-//        mvc.perform(delete(VERSION + USERS + user.getId())
-//                .with(user("blaze").password("Q1w2e3r4"))
-//                .contentType(APPLICATION_JSON))
-//                .andExpect(status().isOk());
     }
 }
