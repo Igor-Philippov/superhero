7eab6968db0c85ec82802e75fbe3d01b8d51ed5b
Igor-Philippov <45106977+Igor-Philippov@users.noreply.github.com>
2018-11-30 14:42:30 -0500

    Delete OriginalDataRepository.java

--- src/main/java/com/superhero/repository/OriginalDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.superhero.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.superhero.model.OriginalData;
-
-public interface OriginalDataRepository extends PagingAndSortingRepository<OriginalData, Integer> {
-}
