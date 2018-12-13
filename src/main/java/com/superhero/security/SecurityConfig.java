aa1bbb3784d82ed1380dbca030a9f0fbd90e3501
Igor-Philippov <45106977+Igor-Philippov@users.noreply.github.com>
2018-12-13 16:34:15 -0500

    Add files via upload

--- src/main/java/com/superhero/security/SecurityConfig.java
+++ src/main/java/com/superhero/security/SecurityConfig.java
@@ -4,10 +4,7 @@ import static com.superhero.rest.constant.Paths.MISSIONS_DELETED;
 import static com.superhero.rest.constant.Paths.MISSION_BY_ID;
 import static com.superhero.rest.constant.Paths.SUPERHERO_BY_ID;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,27 +15,30 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.superhero.config.ApplicationConfig;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private ApplicationConfig config;
 
-    @Value("${spring.security.username}")
-    private String username;
+	@Autowired
+	public void configAuthenticationProvider(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = passwordEncoder();
 
-    @Value("${spring.security.password}")
-    private String password;
-
-    @Value("${spring.security.role}")
-    private String role;
-    //private List<String> roles;
-
-    @Autowired
-    public void configAuthenticationProvider(AuthenticationManagerBuilder auth) throws Exception {
-    	
-        BCryptPasswordEncoder encoder = passwordEncoder();
-        //auth.inMemoryAuthentication().withUser(username).password(encoder.encode(password)).roles(roles.toArray(new String[0]));
-        auth.inMemoryAuthentication().withUser(username).password(encoder.encode(password)).roles(role);
-    }
+		auth.inMemoryAuthentication()
+			.withUser(config.getSpringSecurityUserName())
+			.password(encoder.encode(config.getSpringSecurityUserPwd()))
+			.roles(config.getSpringSecurityRole());
+		
+//		// Securizing actuator endpoints
+//		auth.inMemoryAuthentication()
+//		    .withUser("act_user")
+//		    .password(encoder.encode("act_password"))
+//		    .roles("ACTRADMIN");
+	}
     
 //    @Autowired
 //    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
@@ -53,17 +53,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-         http
-        .csrf().disable()
-        .authorizeRequests()
-          .antMatchers(HttpMethod.DELETE, MISSION_BY_ID).authenticated()
-          .antMatchers(HttpMethod.GET, MISSIONS_DELETED).authenticated()
-          .antMatchers(HttpMethod.DELETE, SUPERHERO_BY_ID).authenticated()
-          .anyRequest().permitAll()
-          .and()
-          .httpBasic()
-          .and()
-          .csrf().disable();
+         http.csrf().disable()
+             .authorizeRequests()
+             .antMatchers(HttpMethod.DELETE, MISSION_BY_ID).authenticated()
+             .antMatchers(HttpMethod.GET, MISSIONS_DELETED).authenticated()
+             .antMatchers(HttpMethod.DELETE, SUPERHERO_BY_ID).authenticated()
+             .anyRequest().permitAll()
+             .and().httpBasic();
+//         .and()
+//         .csrf().disable();
     }
 
     @Override
